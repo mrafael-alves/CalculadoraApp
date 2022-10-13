6c5f5247b50f5d9ffc28a9f5aca1610ff9877f88
Rafael M <magalhaesrafael07@gmail.com>
2022-10-13 14:35:41 -0300

    Implementa: - função decimal(), que adiciona o separador decimal aos números digitados na caixa de input de texto;

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ Builder.load_file('tela.kv')
 # Classe contendo funções da calculadora
 class Calculadora(Widget):
 
-    # função que limpa o espaço de entra de texto
+    # Função que limpa o espaço de entra de texto
     def clear(self):
         self.ids.calc_input.text = '0'
 
-    # função que registra o pressionar de uma tecla numérica
+    # Função que registra o pressionar de uma tecla numérica
     def btPress(self, button):
         previous = self.ids.calc_input.text
 
@@ -27,10 +27,32 @@ class Calculadora(Widget):
         else:
             self.ids.calc_input.text = f'{previous}{button}'
 
-    # função que registra os sinais de operações matemáticas (+,-,x,/)
+    # Função que registra os sinais de operações matemáticas (+,-,x,/)
     def operation(self, sign):
         previous = self.ids.calc_input.text
         self.ids.calc_input.text = f'{previous}{sign}'
+
+    # Função que adiciona o separador decimal aos números
+    def decimal(self):
+        previous = self.ids.calc_input.text
+        signs = ('+', '-', '/', '*')
+
+        for sign in signs:
+            # Caso haja operação na string E o último número NÃO tenha decimal
+            if sign in previous and '.' not in previous.split(sign)[-1]:
+                previous = f'{previous}.'
+                self.ids.calc_input.text = previous
+            # Caso já exista o sepador decimal
+            elif '.' in previous:
+                pass
+            # Caso seja o primeiro número digitado
+            else:
+                previous = f'{previous}.'
+                self.ids.calc_input.text = previous
+
+
+
+
 
 
 
